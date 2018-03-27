9f3fd0ab23dc6ec0c01a870d4eb61d7fba3ae277
keannaanglin <30633496+keannaanglin@users.noreply.github.com>
2018-03-26 19:17:32 -0700

    committing recent changes
    everything compiling, spitting out numbers, but numbers are off...

--- CMSI-186_Projects/homework 05/SoccerSim.java
+++ CMSI-186_Projects/homework 05/SoccerSim.java
@@ -1,36 +1,159 @@
-public class SoccerSim {
+import java.text.DecimalFormat;
 
-    public SoccerSim() {
-        super;
-    }
+ public class SoccerSim {
 
-    public void handleInitialArguments( String args[] ) {
-        System.out.println("\n   Hello world, from the SoccerSim program!!\n\n")
-        if( 0 == args.length ) {
-            System.out.println(" Sorry you must enter at least 8 arguments\n" +
-            " Usage: java SoccerSim <xLocation1> <yLocation1> <xVelocity1> <yVelocity1>  ")
-                               " Usage: java SoccerSim <xLocation1> <yLocation1> <xVelocity1> <yVelocity1> <xLocation2> <yLocation2> <xVelocity2> <yVelocity2> [timeSlice]\n" +
-                               " Please try again...");
-            System.exit(1);
-        }
-        balls = new Ball[];
-        int j = 0;
-        if( (args.length % 4) == 1) {
-            for (int i = 0; i < args.length; i+=4) {
-                balls[j] = new Ball(
-                Double.parseDouble(args[i+0]);
-                Double.parseDouble(args[i+1]);
-                Double.parseDouble(args[i+2]);
-                Double.parseDouble(args[i+3]);
-                );
+     public static double xLocation;
+     public static double yLocation;
+     public static double xVel;
+     public static double yVel;
+     public static double timeSlice = 1.0;
+     private static int ballQuantity;
+     private int FOUR = 4;
+     public static double fieldX = 500;
+     public static double fieldY = 500;
+     public static double poleX = 20;
+     public static double poleY = 20;
+     private final static double DEFAULT_TIME_SLICE_IN_SECONDS = 1.0;
 
-                 xPosition = Double.parseDouble(args[i]);
-                 yPostition = Double.parseDouble(args[i+1]);
-                 xVel = Double.parseDouble(args[i+2]);
-                 yVel = Double.parseDouble(args[i+3]);
+     private static Timer timer;
+     private static Ball[] balls;
+
+     public SoccerSim() {
+         super();
+     }
+
+     public double validateInitialArguments( String[] argValues) throws IllegalArgumentException, NumberFormatException {
+       try {
+         xLocation = Double.parseDouble(argValues[0]);
+         yLocation = Double.parseDouble(argValues[1]);
+         xVel = Double.parseDouble(argValues[2]);
+         yVel = Double.parseDouble(argValues[3]);
+       }
+       catch( NumberFormatException nfe) {
+         System.out.println(nfe.toString());
+       }
+       if (timeSlice > 1800) {
+         throw new IllegalArgumentException("Not a valid timeSlice");
+       }
+       if (xLocation > fieldX || yLocation > fieldY ) {
+         throw new IllegalArgumentException("Location out of range");
+       }
+
+       if ((argValues.length % 4) == 1) {
+         ballQuantity = (argValues.length - 1) / 4;
+       }
+       if ((argValues.length % 4) == 0) {
+         ballQuantity = argValues.length / 4;
+       } else if ((argValues.length % 4) == 2) {
+         throw new IllegalArgumentException("Invalid number of arguments");
+       } else if ((argValues.length % 4) == 3) {
+         throw new IllegalArgumentException("Invalid number of arguments");
+       }
+       return ballQuantity;
+
+     }
+     public boolean isMovement() {
+       for (int i = 0 ; i < ballQuantity; i++) {
+         if (!(balls[i].xVel < 0.000001 && balls[i].yVel < 0.000001)) {
+           return true;
+         }
+       }
+       return false;
+     }
+
+     public boolean isOnField() {
+       for (int i =0 ; i < ballQuantity; i++) {
+         if (balls[i].xLocation < (fieldX / 2) || balls[i].yLocation < (fieldY / 2 )) {
+           return true;
+         }
+       }
+       return false;
+     }
+
+     public void checkForCollision() {
+
+     }
+
+     public void handleInitialArguments( String args[] ) {
+         System.out.println("\n   Hello world, from the SoccerSim program!!\n\n");
+         if( 0 == args.length ) {
+           System.out.println( "   Sorry you must enter at least one argument\n" +
+                               "   Usage: java SoccerSim <x Location> <y Location> <x velocity> <y velocity> [timeSlice]\n" +
+                               "   Please try again..........." );
+             System.exit(1);
+         }
+         if ((args.length % 4) == 1) {
+           timeSlice = Double.parseDouble(args[args.length - 1]);
+         }
+         else {
+           timeSlice = DEFAULT_TIME_SLICE_IN_SECONDS;
+         }
+         timer = new Timer(timeSlice);
+
+         try {
+           int j = 0;
+
+           balls = new Ball[ballQuantity];
+           for (int i = 0; i < args.length/FOUR; i++) {
+             balls[j] = new Ball(this.timeSlice);
+             balls[j].xLocation = Double.parseDouble(args[i]);
+             balls[j].yLocation = Double.parseDouble(args[4 * i + 1]);
+             balls[j].xVel = Double.parseDouble(args[4 * i + 2]);
+             balls[j].yVel = Double.parseDouble(args[4 * i +3 ]);
+             j++;
             }
-            timeSlice = Double.parseDouble(args[args.length - 1]);
-        }
+         }
+         catch( NumberFormatException nfe ) {
+           System.out.println(nfe.toString());
+         }
 
-    }
-}
+     }
+
+
+
+     public static void main( String[] args ) {
+
+       SoccerSim ss = new SoccerSim();
+       DecimalFormat df = new DecimalFormat("#0.000");
+       ss.checkForCollision();
+       try {
+         ss.validateInitialArguments( args );
+       }
+       catch( IllegalArgumentException iae) {
+         System.out.println(iae.toString());
+         System.exit(-1);
+       }
+       System.out.println("Test");
+       System.out.println(ballQuantity);
+       ss.handleInitialArguments( args );
+       System.out.println("INITIAL REPORT at " + timer.toString());
+          for ( int i = 0; i < ballQuantity; i++ ) {
+            System.out.println(i + ": position <" + df.format(balls[i].xLocation) + "," + df.format(balls[i].yLocation)+ ">" );
+            System.out.println(i + ": velocity <" + df.format(balls[i].xVel) + "," + df.format(balls[i].yVel) + ">");
+          }
+          System.out.println("Pole is located at (" + poleX + "," + poleY + ")\n");
+
+
+       while ( ss.isMovement() && ss.isOnField()) {
+         System.out.println(timer.tick());
+         System.out.println("PROGRESS REPORT at" + timer.toString());
+
+         for ( int i = 0; i < ballQuantity; i++ ) {
+           balls[i].move();
+           System.out.println( i + "position <" + df.format(balls[i].getLocation()[0]) + "," + df.format(balls[i].move()[1]) + ">");
+            if(!(balls[i].xVel < 0.000001 && balls[i].yVel < 0.000001))  {
+                System.out.println( i + "velocity <" + df.format(balls[i].xVel) + "," + df.format(balls[i].yVel) + ">");
+            }
+            else {
+                System.out.println("       AT REST");
+            }
+          }
+         }
+         ss.checkForCollision();
+
+       System.out.println("FINAL REPORT at" +  timer.toString() );
+       System.out.println("no collision possible!");
+       System.exit(-1);
+     }
+
+ }
--- CMSI-186_Projects/homework 05/Timer.java
+++ CMSI-186_Projects/homework 05/Timer.java
@@ -1,26 +1,61 @@
+
+import java.text.DecimalFormat;
+
 public class Timer {
 
-    public double totalSeconds;
-    public double hours, minutes, seconds;
-    public double timeslice;
+  private static final double DEFAULT_TIME_SLICE_IN_SECONDS = 1.0;
+  private static double totalSeconds = 0;
+  private static double timeSlice = 0;
+  private double hours = 0;
+  private double minutes = 0;
+  private double seconds = 0;
 
-    public Timer( double t ){
-        totalSeconds = 0.0;
-        timeslice = t;
-    }
+  public Timer() {
 
-    //methods start there
+  }
+  public Timer(double t) {
+    timeSlice = t;
+  }
 
-    public double secondsElapsed() {
-        totalSeconds += timeslice;
-        return totalSeconds;
-    }
+  public double tick() {
+    totalSeconds += timeSlice;
+    return totalSeconds;
+  }
 
-    public String toString() {
-        hours = Math.floor(totalSeconds / 3600);
-        minutes = Math.floor((totalSeconds % 3600) /60);
-        seconds = totalSeconds - ((minutes * 60) + (hours * 3600));
-        DecimalFormat df = new DecimalFormat("#.00");
-        return (int) hours + " : " + (int) minutes + " : " + df.format(seconds);
-    }
+  /**
+   * Method to validate the optional time slice arg from the command line
+   * @param argValue String from the main programs args[7] input
+   * @return double-precision value of the argument or -1.0 if invalid
+   * @throws NumberFormatException
+   */
+  public double validateTimeSliceArg(String argValue) throws NumberFormatException,
+                                                             IllegalArgumentException {
+      double returnValue = 0.0;
+      try {
+         returnValue = Double.parseDouble( argValue );
+      }
+      catch( NumberFormatException nfe ) {
+         throw new NumberFormatException( " [converting time slice value failed]");
+      }
+      if( (0.0 >= returnValue) || (1800.0 < returnValue) ) {
+         throw new IllegalArgumentException( " [time slice value out of range]" );
+      }
+      return returnValue;
+   }
+
+
+
+  public double getTotalSeconds() {
+    return totalSeconds;
+  }
+
+  public String toString() {
+    DecimalFormat df = new DecimalFormat("#.000");
+    DecimalFormat dfi = new DecimalFormat("#0");
+    hours = Math.floor( totalSeconds / 3600);
+    minutes = Math.floor((totalSeconds % 3600) / 60);
+    seconds = totalSeconds - ((hours * 3600) + (minutes * 60));
+    return dfi.format(hours) + ":" + dfi.format(minutes) + ":" + df.format(seconds);
+  }
+
 }
